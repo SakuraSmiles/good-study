584b4cbd6c601c9f8d4bb56ba3033ae8924eb9f2
juzou.wangchengbing <juzou@tuya.com>
2022-03-10 16:11:39 +0800

    排序接口实现

---
+++ src/sort/ISort.java
@@ -0,0 +1,12 @@
+package sort;
+
+public interface ISort {
+    /**
+     * 整形数组从小到大排序
+     *
+     * @param nums 排序数组
+     * @author wangcb （juzou@tuya.com）
+     * @date 4:06 下午 2022/3/10
+     **/
+    void sort(int[] nums);
+}
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,21 +1,14 @@
 package sort;
 
-import java.util.Arrays;
-
 /**
  * 选择排序
  *
  * @author wangcb （juzou@tuya.com）
  * @date 3:05 下午 2022/3/10
  **/
-public class SelectionSort {
-    public static void main(String[] args) {
-        int[] nums = new int[]{5, 10, 20, 1, 5, -10, 100, 21};
-        sort(nums);
-        System.out.println(Arrays.toString(nums));
-    }
-
-    public static void sort(int[] nums) {
+public class SelectionSort implements ISort {
+    @Override
+    public void sort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < nums.length; j++) {
