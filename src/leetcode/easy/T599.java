9c09f45e5770186c564395a21ea38b86b695ef79
juzou.wangchengbing <juzou@tuya.com>
2022-03-14 17:27:49 +0800

    力扣599题

---
+++ src/leetcode/easy/T599.java
@@ -0,0 +1,65 @@
+package leetcode.easy;
+
+import java.util.*;
+
+/**
+ * 两个列表的最小索引总和
+ * <p>
+ * 假设 Andy 和 Doris 想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+ * 你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设答案总是存在。
+ * <p>
+ * 示例 1:
+ * 输入: list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，
+ * list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+ * 输出: ["Shogun"]
+ * 解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+ * <p>
+ * 示例 2:
+ * 输入:list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，
+ * list2 = ["KFC", "Shogun", "Burger King"]
+ * 输出: ["Shogun"]
+ * 解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+ *
+ * @author wangcb （juzou@tuya.com）
+ * @date 3:45 下午 2022/3/14
+ **/
+public class T599 {
+    public static void main(String[] args) {
+        String[] list1 = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] list2 = {"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"};
+        for (String v : findRestaurant(list1, list2)) {
+            System.out.println(v);
+        }
+    }
+
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+        List<String> list = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>(16);
+        HashMap<String, Integer> result = new HashMap<>(16);
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+        for (int i = 0; i < list2.length; i++) {
+            String name = list2[i];
+            if (map.containsKey(name)) {
+                result.put(name, map.get(name) + i);
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> it = result.entrySet().iterator();
+        Integer min = Integer.MAX_VALUE;
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value < min) {
+                min = value;
+                list.clear();
+                list.add(key);
+            } else if (value.equals(min)) {
+                list.add(key);
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+}
